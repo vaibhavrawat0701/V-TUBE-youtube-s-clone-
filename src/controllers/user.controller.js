2187a61327d5f98da671eae8db6d0b7e39bdcb7a
Vaibhav Rawat <vaibhavrawat0701@gmail.com>
2026-09-04 23:52:50 +0530

    implement user registration logic and add multer middleware for file uploads

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,22 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-
+import { ApiError } from "../utils/ApiError.js";
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    messsage: "OK",
-  });
+  //get user details from frontend
+  //validation - not empty
+  //check if user already exists : username, email
+  //check for images, check for avatar
+  //upload them to cloudinary, avatar
+  //create user to object - create entry in db
+  //remove password from response and refresh token fields from response
+  //check for user creation
+  //return response
+
+  const { fullName, email, username, password } = req.body;
+  console.log("email", email);
+
+  if (fullName === "") {
+    throw new ApiError(400, "fullName is required");
+  }
 });
 
 export { registerUser };
