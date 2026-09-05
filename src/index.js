a24301b62f286880a62d6c7a0b4fb346305ae934
Vaibhav Rawat <vaibhavrawat0701@gmail.com>
2026-09-05 23:59:48 +0530

    fix: streamline dotenv configuration by using direct import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import dotenv from "dotenv";
+import "dotenv/config";
 import { app } from "./app.js";
 import connectDB from "./db/index.js";
-dotenv.config({ path: "./.env" });
 
 connectDB()
   .then(() => {
