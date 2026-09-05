a221fe8e0b400769600d075ef5d7f4c7a7eba4e3
Vaibhav Rawat <vaibhavrawat0701@gmail.com>
2026-09-05 22:23:47 +0530

    update user registration logic to use consistent cloudinary upload function; fix import path for multer middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
-import { uploadImageToCloudinary } from "../utils/cloudinary.js";
+import { uploadCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
@@ -48,8 +48,8 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Avatar is required");
   }
   //5
-  const avatar = await uploadOnCloudinary(avatarLocalPath);
-  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+  const avatar = await uploadCloudinary(avatarLocalPath);
+  const coverImage = await uploadCloudinary(coverImageLocalPath);
 
   if (!avatar) {
     throw new ApiError(500, "Failed to upload avatar");
