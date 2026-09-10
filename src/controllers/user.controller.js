da30ae4963b4d0acb62ddca18d7dc5a2e975be3d
Vaibhav Rawat <vaibhavrawat0701@gmail.com>
2026-09-10 23:20:55 +0530

    feat: enhance updateUserAvatar and updateUserCoverImage functions; return updated user data and success messages

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -297,11 +297,18 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Error while uploading on avatar");
   }
 
-  const user = await User.findByIdAndUpdate(req.user?._id, {
-    $set: {
-      avatar: avatar.url,
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        avatar: avatar.url,
+      },
     },
-  });
+    { new: true }
+  ).select("-password");
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Avatar image updated successfully"));
 });
 
 const updateUserCoverImage = asyncHandler(async (req, res) => {
@@ -316,11 +323,19 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Error while uploading the image");
   }
 
-  await User.findByIdAndUpdate(req.user?._id, {
-    $set: {
-      avatar: avatar.url,
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        coverImage: coverImage.url,
+      },
     },
-  });
+    { new: true }
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Cover image updated successfully"));
 });
 export {
   registerUser,
@@ -331,4 +346,5 @@ export {
   changeCurrentPassword,
   updateAccountDetails,
   updateUserAvatar,
+  updateUserCoverImage,
 };
