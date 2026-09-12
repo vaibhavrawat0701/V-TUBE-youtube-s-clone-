ecfc4501eeff3dc656a25f324bb57da6ab823bc8
Vaibhav Rawat <vaibhavrawat0701@gmail.com>
2026-09-12 18:28:32 +0530

    feat: add routes for updating user cover image and retrieving user channel profile and watch history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,6 +7,9 @@ import {
   getCurrentUser,
   updateAccountDetails,
   updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -33,5 +36,11 @@ router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 router
   .route("/avatar")
   .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
