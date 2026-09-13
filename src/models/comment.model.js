dd89fd6d6a804eba34dc6648c8425af3befdb97d
Vaibhav Rawat <vaibhavrawat0701@gmail.com>
2026-09-13 22:50:56 +0530

    feat: add comment, like, playlist, and tweet models

---
+++ src/models/comment.model.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+import { Video } from "./video.model";
+import { User } from "./user.model";
+
+const commentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    Video: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
