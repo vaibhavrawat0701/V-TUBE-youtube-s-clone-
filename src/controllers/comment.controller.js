3db0f1717c412b5771849cca3a2af4f007d3b823
Vaibhav Rawat <vaibhavrawat0701@gmail.com>
2026-09-15 23:16:43 +0530

    fix: correct variable naming in getVideoComments and addComment functions; implement updateComment logic with error handling

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -13,7 +13,7 @@ const getVideoComments = asyncHandler(async (req, res) => {
   if (!mongoose.isValidObjectId(videoId)) {
     throw new ApiError(400, "Invalid video ID");
   }
-  const comment = await Comment.find({
+  const comments = await Comment.find({
     video: videoId,
   })
     .populate("owner", "username fullName avatar")
@@ -52,7 +52,7 @@ const addComment = asyncHandler(async (req, res) => {
     throw new ApiError(400, "comment content is required");
   }
 
-  const comment = await Comment.create({
+  const comments = await Comment.create({
     content: content.trim(),
     video: videoId,
     owner: req.user._id,
@@ -61,4 +61,37 @@ const addComment = asyncHandler(async (req, res) => {
   return res
     .status(201)
     .json(new ApiResponse(201, comment, "comment added successfully"));
+});
+
+//updating the comments
+
+const updateComment = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+  const { content } = req.body;
+
+  if (!mongoose.isValidObjectId(commentId)) {
+    throw new ApiError(400, "Invalid comment id");
+  }
+  if (!content || !content.trim()) {
+    throw new ApiError(400, "Comment content is required");
+  }
+
+  const comment = await Comment.findById(commentId);
+
+  if (!comment) {
+    throw new ApiError(404, "Comment not found");
+  }
+
+  //Only owner can update the comment
+
+  if (comment.owner.toString() !== req.user._id.toString()) {
+    throw new ApiError(403, "You are not allowed to update this comment");
+  }
+  comment.content = content.trim();
+
+  await comment.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, comment, "Comments uplaod successfully"));
 });
