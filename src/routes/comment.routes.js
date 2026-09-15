cb1ca03acb2b08db1d2efea6144644dfac074527
Vaibhav Rawat <vaibhavrawat0701@gmail.com>
2026-09-15 23:48:39 +0530

    style: format and organize imports in comment.routes.js for improved readability

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,4 +1,20 @@
-import {Router} from 'express';
- import{
-  addComment,deleteComment,getVideoComments,updateComment
- }
+import { Router } from "express";
+import {
+  addComment,
+  deleteComment,
+  getVideoComments,
+  updateComment,
+} from "../controllers/comment.controller.js";
+
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.route("/:videoId").get(getVideoComments).post(verifyJWT, addComment);
+
+router
+  .route("/c/:commentId")
+  .patch(verifyJWT, updateComment)
+  .delete(verifyJWT, deleteComment);
+
+export default router;
