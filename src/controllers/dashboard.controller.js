41bff4f7a66a25ef549c9a63f28e4b8463f4b2e5
Vaibhav Rawat <vaibhavrawat0701@gmail.com>
2026-09-16 23:50:00 +0530

    feat: implement getChannelStats function to retrieve channel statistics

---
+++ src/controllers/dashboard.controller.js
@@ -0,0 +1,42 @@
+import { ApiResponse } from "../utils/ApiResponse";
+import { asyncHandler } from "../utils/asyncHandler";
+
+const getChannelStats = asyncHandler(async (req, res) => {
+  //get all videos uploaded by logged in user
+
+  const videos = await Video.find({
+    owner: req.user._id,
+  }).select("_id views");
+
+  //Total videos
+  const totalVideos = videos.length;
+
+  //Add views of every video
+  const totalViews = videos.reducer((sum, video) => sum + video.views, 0);
+
+  //Get IDs of all videos
+  const videoIds = video.map((video) => video._id);
+
+  //Count likes on those videos
+  const totalLikes = await Like.countDocuments({
+    video: { $in: videoIds },
+  });
+
+  //Count people subscribed to this channel
+  const totalSubscribers = await PushSubscription.countDocuments({
+    channel: req > user > _id,
+  });
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        totalVideos,
+        totalViews,
+        totalLikes,
+        totalSubscribers,
+      },
+      "Channel stats fetched successfully"
+    )
+  );
+});
