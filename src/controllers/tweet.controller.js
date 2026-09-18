3e7704bb233ed9f437f1f183c4a24dc6f779d3dc
Vaibhav Rawat <vaibhavrawat0701@gmail.com>
2026-09-18 22:54:20 +0530

    feat: add getUserTweets function to fetch all tweets created by a user

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -24,3 +24,33 @@ const createTweet = asyncHandler(async (req, res) => {
     .status(201)
     .json(new ApiResponse(201, tweet, "Tweet created successfully"));
 });
+
+//get user tweets
+
+const getUserTweets = asyncHandler(async (req, res) => {
+  const { userId } = req.params;
+
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(400, "Invalid user id");
+  }
+
+  //  if user exists
+  const user = await User.findById(userId);
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  // Find all tweets created by this user
+  const tweets = await Tweet.find({
+    owner: userId,
+  })
+    .populate("owner", "username fullName avatar")
+    .sort({
+      createdAt: -1,
+    });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, tweets, "User tweets fetched successfully"));
+});
