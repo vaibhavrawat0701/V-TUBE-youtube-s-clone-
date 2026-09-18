f7fdab66666bb028ac99f3faa43851f5fca6651c
Vaibhav Rawat <vaibhavrawat0701@gmail.com>
2026-09-18 22:17:14 +0530

    feat: add getUserChannelSubscribers function to fetch subscribers for a channel

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -35,3 +35,21 @@ const toggleSubscription = asyncHandler(async (req, res) => {
       new ApiResponse(200, { subscribed: true }, "Subscribed successfully")
     );
 });
+
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+  const { channelId } = req.params;
+
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, "Invalid channel id");
+  }
+
+  const subscribers = await Subscription.find({
+    channel: channelId,
+  }).populate("subscriber", "username fullName avatar");
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, subscribers, "Subscribers fetched successfully")
+    );
+});
