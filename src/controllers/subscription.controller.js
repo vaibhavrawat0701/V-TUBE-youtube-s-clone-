d47a8485896e8f9529570a638cad5e8e4b88ffd0
Vaibhav Rawat <vaibhavrawat0701@gmail.com>
2026-09-18 22:35:31 +0530

    feat: add getSubscribedChannels function to fetch channels a user is subscribed to

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,3 +1,10 @@
+import mongoose, { isValidObjectId } from "mongoose";
+import { User } from "../models/user.model.js";
+import { Subscription } from "../models/subscription.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
 const toggleSubscription = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
 
@@ -53,3 +60,26 @@ const getUserChannelSubscribers = asyncHandler(async (req, res) => {
       new ApiResponse(200, subscribers, "Subscribers fetched successfully")
     );
 });
+
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+  const { subscriberId } = req.params;
+
+  if (!isValidObjectId(subscriberId)) {
+    throw new ApiError(400, "Invalid subscriber id");
+  }
+
+  const subscribedChannels = await Subscription.find({
+    subscriber: subscriberId,
+  }).populate("channel", "username fullName avatar");
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        subscribedChannels,
+        "Subscribed channels fetched successfully"
+      )
+    );
+});
+export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
