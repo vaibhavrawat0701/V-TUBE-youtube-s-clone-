df63ba1b57c404688d464e73a5f393a30dec6eda
Vaibhav Rawat <vaibhavrawat0701@gmail.com>
2026-09-22 20:33:36 +0530

    feat: remove watchHistory field from user schema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -32,12 +32,7 @@ const userSchema = new Schema(
     coverImage: {
       type: String, //cloudinary url
     },
-    watchHistory: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Video",
-      },
-    ],
+
     password: {
       type: String,
       required: [true, "Password is required"],
